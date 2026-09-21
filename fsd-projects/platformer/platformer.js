738c2343acf31b55f1cae1e79eaebe2c6ad4eac2
tihannasinclair <tihanna.sinclair@sarahtreed.org>
2026-09-21 18:00:52 +0000

    finished game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -32,21 +32,24 @@ $(function () {
 
     // TODO 2 - Create Platforms
 createPlatform(500, 630, 50, 290, "black");
-createPlatform(700, 400, 50, 50, "purple");
-createPlatform(900, 630, 50, 290, "blue")
-createPlatform(1200, 630, 50, 290, "pink")
-createPlatform(1400, 630, 50, 290, "red")
+createPlatform(700, 630, 50, 290, "purple");
+createPlatform(900, 630, 50, 290, "blue");
+createPlatform(1100, 630, 50, 290, "pink");
+createPlatform(1300, 630, 50, 290, "red");
 
     // TODO 3 - Create Collectables
-
+createCollectable("kennedi", 1270, 700);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
+createCollectable("steve", 550, 90, 1, 0.5);
 
 
     
     // TODO 4 - Create Cannons
-
-
-    
-    
+createCannon("top", 300, 700);
+createCannon("top", 650, 700);
+createCannon("top", 930, 700);
+createCannon("right", 650, 2500); 
+createCannon("right", 450, 2500);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
